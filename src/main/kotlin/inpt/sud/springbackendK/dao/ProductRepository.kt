f2527439914e21f92b13a55ee959fa8fa3b9347a
salma-hbh <elhabhoubsalma2@gmail.com>
2022-03-07 22:38:15 +0100

    Initial commit

---
+++ src/main/kotlin/inpt/sud/springbackendK/dao/ProductRepository.kt
@@ -0,0 +1,15 @@
+package inpt.sud.springbackendK.dao
+
+import inpt.sud.springbackendK.data.Product
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.query.Param
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.data.rest.core.annotation.RestResource
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+
+@CrossOrigin("*")
+@RepositoryRestResource
+interface ProductRepository: JpaRepository<Product?, Long?> {
+
+}
